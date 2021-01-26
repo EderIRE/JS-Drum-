59b55849d857be73981f526ea36a6a57c219ec7e
EderIRE <71097226+EderIRE@users.noreply.github.com>
2021-01-26 08:45:41 +0000

    click added

--- script.js
+++ script.js
@@ -12,7 +12,19 @@ function removeTransition(e) {
   this.classList.remove('playing');
 }
 
+function playSoundClick(e) {
+  const keyCode = this.getAttribute('data-key');
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
+}
+
 const keys = document.querySelectorAll('.key');
+
 keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
+keys.forEach((key) => key.addEventListener('click', playSoundClick));
 
 window.addEventListener('keydown', playSound);
